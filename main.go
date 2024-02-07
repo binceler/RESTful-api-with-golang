e6c7b56f572ed087cec464750317322d284b4326
binceler <busrainceler@gmail.com>
2024-02-07 22:21:21 +0300

    first commit for project

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import "net/http"
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is about!"))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to index page!"))
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	/*http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("Merhaba Mars!"))
+	})*/
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/index", indexHandler)
+	http.HandleFunc("/about", aboutHandler)
+	http.ListenAndServe(":9999", nil)
+}
